18c626f1d03fbffc10c78c2515936ebdc052ae3f
Alexey Ryabov <lesha_12_01@mail.ru>
2023-06-04 14:15:56 +0300

    chore: setup Prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  bracketSpacing: true,
+  singleQuote: true,
+  bracketSameLine: false,
+  printWidth: 80,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
